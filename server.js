0f0bedaba8afd803d9e3f063741a7193e429ae8f
Yan <young69budda@gmail.com>
2023-11-14 04:18:37 +0200

    new1

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
 
-// const DB_HOST =
-//   "mongodb+srv://admin:adminadmin@cluster0.fumjo3k.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const DB_HOST =
+  "mongodb+srv://admin:adminadmin@cluster0.fumjo3k.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-const { DB_HOST, PORT = 3000 } = process.env.DB_HOST;
+// const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
-    console.log(`Database connection successful. PORT = ${PORT}`);
+    app.listen(3000);
+    console.log(`Database connection successful. PORT = 3000`);
   })
   .catch((error) => {
     console.log(error.message);
